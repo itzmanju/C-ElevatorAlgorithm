f228a70be5c43152a066f51feeb011a14a7a64c9
Manju Venugopal <itzmanju@gmail.com>
2012-08-23 09:26:46 +0800

    Initial version
    Computing nearest lift level, Calculating and setting defaults per the
    time of the day

---
+++ queue.c
@@ -0,0 +1,11 @@
+
+#include "ElevatorAlgorithmInC.h"
+#include "queue.h"
+
+EXT_INPUT pop_queue()
+{
+	EXT_INPUT in;
+	in.cur_user_level = 8;
+	in.user_input_direction = DIR_DOWN;
+	return in;
+}
---
+++ queue.h
@@ -0,0 +1,4 @@
+
+
+// User input from outside the elevator is queued and we get it by using the queue functions
+EXT_INPUT pop_queue();
